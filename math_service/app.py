a6192e6cef6e940a876467bcba882579daaf6326
emiliosilvaroli <emiliosilvaroli99@gmail.com>
2025-03-26 20:52:33 +0100

    yee

--- math_service/app.py
+++ math_service/app.py
@@ -4,10 +4,10 @@ app = Flask(__name__)
 
 @app.route('/math/add')
 def add():
-    # Nessun try-except: input errati causano crash
-    a = float(request.args.get('a'))  # se manca o è errato => ValueError
-    b = float(request.args.get('b'))
-    return f"Risultato: {a + b}"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    try:
+        a = float(request.args.get('a'))
+        b = float(request.args.get('b'))
+        return f"Risultato: {a + b}"
+    except:
+        print("Errore irreversibile: uscita immediata.")
+        os._exit(1)
